d80db66c91d371352dfc3c031ac3cbec8b25954c
Azizahaul <azizahauliarhamid@gmail.com>
2025-05-27 15:26:30 +0700

    int main

--- agregasi.cpp
+++ agregasi.cpp
@@ -2,5 +2,23 @@
 #include <vector>
 using namespace std;
 
-#include "anak.h"
-#include "ibu.h"
+#include "anak.hpp"
+#include "ibu.hpp"
+
+int main(){
+    ibu* varibu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varanak1 = new anak("tono");
+    anak* varanak2 = new anak("rini");
+    anak* varanak3 = new anak("dewi")
+
+    varibu->tambahAnak(varanak1);
+    varibu->tambahAnak(varanak2);
+    varibu2->tambahAnak(varanak3);
+    varibu2->tambahAnak(varanak1);
+
+    varibu->cetakAnak();
+    varibu->cetakAnak();
+
+
+}
